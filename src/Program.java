0d9cb99eb9aec2bb1940700fe4274451d5a17a30
AndreyBeny <dubina2017@yandex.ru>
2022-05-24 20:38:13 +0300

    create method getElementsByWord

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class Program {
     public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("text");
+        arr.add("no this word");
+        arr.add("text one");
+        arr.add("text two");
+        arr.add("five");
+        arr.add("three");
+        System.out.println(getElementsByWord(arr, "text"));
+    }
 
+    static ArrayList<String> getElementsByWord(ArrayList<String> arrayList, String word){
+        return (ArrayList<String>) arrayList.stream().filter(a -> a.toUpperCase()
+                .contains(word.toUpperCase())).collect(Collectors.toList());
     }
 }
